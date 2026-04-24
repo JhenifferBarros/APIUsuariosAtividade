c3e32c9b00741ad7632791e7aa710e8c75693e8e
JhenifferBarros <jheniffer.barros2001@gmail.com>
2026-04-23 21:30:11 -0300

    Upload projeto

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,59 @@
+﻿using APIUsuarios.DAO;
+using APIUsuarios.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIUsuarios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+
+        public IActionResult Listar() 
+        {
+            var dao = new UsuarioDAO();
+            var usuarios = dao.ListarUsuarios();
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("cadastrar")]
+
+        public IActionResult Cadastrar([FromBody] UsuarioDTO usuario)
+        {
+            var dao = new UsuarioDAO();
+            dao.CadastrarUsuario(usuario);
+
+            return Ok("Usuário cadastrado com sucesso!");
+        }
+
+        [HttpPut]
+        [Route("atualizar/{id}")]
+        public IActionResult Atualizar([FromRoute] int id, [FromBody] UsuarioDTO usuario)
+        {
+            var dao = new UsuarioDAO();
+            var linhasAfetadas = dao.AtualizarUsuario(id, usuario);
+
+            if (linhasAfetadas == 0 )
+                return NotFound("Usuário não encontrado.");
+
+            return Ok("Usuário atualizado com sucesso!");
+        }
+
+        [HttpDelete]
+        [Route("deletar/{îd}")]
+        public IActionResult Deletar([FromRoute] int id)
+        {
+            var dao = new UsuarioDAO();
+            var linhasAfetadas = dao.DeletarUsuario(id);
+
+            if (linhasAfetadas == 0)
+                return NotFound("Usuário não encontrado.");
+
+            return Ok("Usuário excluído com sucesso!");
+        }
+    }
+}
